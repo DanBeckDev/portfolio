45624afdbf4450b918429f4144f5cc10df523d37
boreme12 <boreme12@gmail.com>
2019-07-13 16:50:42 +0100

    deployment config

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
   plugins: [
     new HtmlWebPackPlugin({
       template: './public/index.html',
-      filename: './index.html',
+      filename: '../index.html',
     }),
   ],
   resolve: {
